36bed8b541b80e47443e4a0b314a6d7c1b709845
DannyJoy01 <dannyjoy678@gmail.com>
2025-10-01 16:07:55 +0100

    adjust route

--- backend/src/models/otp/otpToken.js
+++ backend/src/models/otp/otpToken.js
@@ -1,13 +1,16 @@
 import mongoose from "mongoose";
 
-const otpTokenSchema = new mongoose.Schema({
+const otpTokenSchema = new mongoose.Schema(
+  {
     userId: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'User' },
     otpHash: { type: String, required: true },
-    expiresAt: { type: Date, required: true },   
+    expiresAt: { type: Date, required: true },
     used: { type: Boolean, default: false },
     attempts: { type: Number, default: 0 },
-    createdAt: { type: Date, default: () => Date.now() },
-});
+  },
+  { timestamps: true } // <-- adds createdAt & updatedAt automatically
+);
+
 
 const OtpToken = mongoose.model("OtpToken", otpTokenSchema);
 export default OtpToken;
