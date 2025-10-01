f568214ce54a2ce8ff419b676b6317529fee8692
DannyJoy01 <dannyjoy678@gmail.com>
2025-10-01 16:42:48 +0100

    index.js adjust

--- backend/src/index.js
+++ backend/src/index.js
@@ -16,7 +16,7 @@ const app = express();
 dotenv.config();
 
 // defining port
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 if (process.env.ENV === "development") {
    app.use(morgan('dev'));
 }
